53cc9edc43f421d85942e50df418f988e77f5dd3
J.Laro <joselarojr@gmail.com>
2021-02-17 22:38:10 -0500

    added readMe, manifest.webmanifest, service-worker.js. Appears that I am still getting errors regarding the inability to execute 'cache' on 'addAll'. Will have to look into this

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # budget-tracker
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+## _Description_
+
+This application assist the user by tracking their budget. With the easy to use functionality, all the user has to do is enter the name of their transaction - as well as the amount. Then buy clicking "add funds" will result in an increase to their total or "subtract" which will decrease the total. A dynamic graph measures their transactions as well displays all previous transactions. 
+
+In the event that the user loses internet connection - FEAR NOT - their data is safely stored inside IndexedDB pending, once the user is reconnected the pending data is extracted from IndexedDB. 
+
+## _Technologies_
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=JRLaro&theme=tokyonight)
+
+## _Usage_
+
+The application can be viewed from here:
+
+> https://aqueous-thicket-74160.herokuapp.com//
+
+![](public/icons/screenShot.png)
+
+
+## _Author Info_
+
+> Email: joselarojr@gmail.com
+
+- GitHub User: JRLaro
+
+  ![GitHub stats](https://github-readme-stats.vercel.app/api?username=JRLaro&show_icons=true&theme=tokyonight)
+
+## _Credits // License_
+
+No collaborators were harmed in the making of this application
+
+> MIT License
+> Copyright (©) 2020, J.Laro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,95 @@
-// console.log("Hello from service worker!");
+console.log("Hello from service worker!");
 
-// const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v2';
+const RUNTIME_CACHE = "runtime-cache";
 // const DATA_CACHE_NAME = 'data-cache-v1'
 
-// const urlsToCache = [
-//   '/',
-//     'public/index.html',
-//   'public/styles.css',
-//     '/manifest.webmanifest',
-//     'public/icons/icon-192.png',
-//   'public/icons/icon-512.png',
-// ];
 
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
+    'public/icons/icon-192.png',
+  'public/icons/icon-512.png',
+];
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         fetch(event.request)
-//             .then(function (response) {
-//                 caches.open(CACHE_NAME).then(function (cache) {
-//                     cache.put(event.request, response);
-//                 });
-//             })
-//             .catch(function () {
-//                 caches.match(event.request).then(function (response) {
-//                     return response;
-//                 }
-//                 );
-//             })
-//     )
-// });
 
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+        .open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    const currentCaches = [CACHE_NAME, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+        .keys()
+        .then(cacheNames => {
+           
+            return cacheNames.filter(
+                cacheName => !currentCaches.includes(cacheName)
+            );
+        })
+        .then(cachesToDelete => {
+            return Promise.all(
+                cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
+        })
+        .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    
+    if (event.request.url.includes("/api/")) {
+    
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+        
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
+        })
+    );
+});
 
